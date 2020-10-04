49f56bd2145714883c3913ba8e3e444bca085939
t3ssk <te.konecna@icloud.com>
2020-10-04 22:31:06 +0200

    add main sections

--- index.js
+++ index.js
@@ -19,3 +19,36 @@ menuOn.addEventListener('click', function(){
 menuClose.addEventListener('click', function(){
   menu.classList.remove('active')
 })
+
+
+//Photos animated in on scroll 
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+window.addEventListener('scroll', debounce(slideIn));
+const imgs = document.querySelectorAll('.images');
+function slideIn(e){
+  if(e){
+    imgs.forEach(image=>{
+      const slideInAt = (window.scrollY+window.innerHeight) - (image.height/2);
+      const isShown = slideInAt > image.offsetTop;
+      if(isShown){
+        image.classList.add('active')
+      }
+    })
+  }
+}
+
+
