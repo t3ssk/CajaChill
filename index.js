ad06ed2eaeed34ea30e5e8794631e176add13c50
t3ssk <te.konecna@icloud.com>
2020-10-04 12:12:00 +0200

    initialize git

---
+++ index.js
@@ -0,0 +1,11 @@
+var options = {
+    "animate": true,
+    "patternWidth": 20.81,
+    "patternHeight": 58.63,
+    "grainOpacity": 0.04,
+    "grainDensity": 1,
+    "grainWidth": 1,
+    "grainHeight": 1
+  }
+
+grained('#main',options)
